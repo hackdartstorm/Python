727cb7767366a2da6334acebb676abea6a19abf2
Pritam <hackdartstorm@gmail.com>
2025-12-16 10:20:49 -0800

    Day 3

---
+++ Basics/esc_sequence.py
@@ -0,0 +1,7 @@
+# "\t" is for tab space and "\n" is for new line 
+name = "Raj\tPal\tYadav is a good boy,\nhe love to read books." 
+
+# If we use double quote or single quote with "\" it act as same 
+Class = "How are you ? \"hope you are fine\" " 
+Roll = 'How are you ? \'hope you are fine\' ' 
+print(name,Class,Roll) 
---
+++ Basics/string-functions.py
@@ -0,0 +1,15 @@
+#String Functions
+name = "ram is a good good Boy 2323"
+
+print(name.capitalize()) #Make the first letter of the word uppercase
+print(name.count("good")) #Counts the given string
+print(name.endswith("Boy")) #check that the given String ends with Boy or not 
+print(name.startswith("Ram")) #check that the given String Starts with Ram or not 
+print(name.find("good")) #return the index of the first occurence of the given Str
+print(name.index("good")) #return the index of the first occurence of the given Str
+print(name.isalnum()) #check the given string is alpha-numeric or not 
+print(name.isspace()) #if the str is empty and minimum there will be one character then return true else false 
+print(name.upper()) #Return a copy of the string converted to uppercase.
+print(name.title()) #Return a version of the string where each word is titlecased
+print(name.split()) #Return a list of the substrings in the string, using sep as the separator string.
+print(name.isupper()) #Return True if the string is an uppercase string, False otherwise.
