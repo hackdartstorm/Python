30ed3fa04aba7ceabcd3da3948d2945d88cf9b7d
Dev <5343rahuldev@gmail.com>
2026-02-23 23:48:39 +0530

    Fix trailing whitespace

--- exercises/1000_programs/medium/1870_min_speed.py
+++ exercises/1000_programs/medium/1870_min_speed.py
@@ -19,7 +19,7 @@ def minSpeedOnTime(dist, hour):
             ans = mid
             right = mid - 1
         else:
-            left = mid + 1     
+            left = mid + 1
     return ans
 # Driver code to test locally
 if __name__ == "__main__":
